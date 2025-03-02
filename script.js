d7403518dec6db0823e9d4698a1af6a097e375aa
Adarsh Ranjan <119723460+rockadarsh@users.noreply.github.com>
2025-03-02 20:14:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-var timer=60;
+var timer;
 var score=0;
 var hitrn=0;
+
 function increasescore()
 {
     score+=10;
     document.querySelector("#scoreval").textContent=score;
 }
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getFirestore, collection, getDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA37H4mNTqGzUXUO2VnfqkesgnstWUvP-0",
+  authDomain: "bubble-hit-49af6.firebaseapp.com",
+  projectId: "bubble-hit-49af6",
+  storageBucket: "bubble-hit-49af6.firebasestorage.app",
+  messagingSenderId: "142433510992",
+  appId: "1:142433510992:web:4851a5d07343e8b3ffd355"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const leaderboardRef = doc(db, "leaderboard", "leaderboardData");
+
+// Function to fetch the leaderboard
+const fetchLeaderboard = () => {
+  return getDoc(leaderboardRef).then((docSnap) => {
+    if (docSnap.exists()) {
+      const leaderboardArr = docSnap.data().leaderboard;
+      return leaderboardArr;
+    } else {
+      return []
+    }
+  }).catch((error) => {
+  });
+};
+
+const saveLeaderboard = (leaderboardArr) => {
+    setDoc(leaderboardRef, { leaderboard: leaderboardArr })
+      .then(() => {
+      })
+      .catch((error) => {
+      });
+};
+
+
+
+
 function getnewhhit()
 {
     hitrn= Math.floor(Math.random()*10);
@@ -18,7 +62,6 @@ for(var i=1;i<=84;i++)
 {
     var num=Math.floor(Math.random()*10);
     clutter+=`<div class="bubble">${num}</div>`;
-
 }
 document.querySelector("#pbtm").innerHTML=clutter;
 }
@@ -26,7 +69,7 @@ function runTimer()
 {
     const difficulty = document.querySelector("#difficulty").value;
     if(difficulty == "extreme-easy") {
-        timer = 30;
+        timer = 10;
     }else if(difficulty === "easy") {
         timer = 60;
     }else if(difficulty === "medium") {
@@ -49,16 +92,20 @@ function runTimer()
             endPage.classList.remove("hidden");
             endPage.querySelector("#finalscore").innerHTML=score;
             // Leaderboard
-            const leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
-            const myName = document.getElementById("name").value || "Unknown"
-            leaderboard.push({name: myName, score: score, playedAt: new Date().toLocaleString(), difficulty: 
-                document.querySelector("#difficulty").options[document.querySelector("#difficulty").selectedIndex].text
-            });
-            leaderboard.sort((a, b) => b.score - a.score);
-            // save top 10 scores
-            localStorage.setItem("leaderboard", JSON.stringify(leaderboard.slice(0, 10)));
-            // Sort leaderboard by score
-            // Display leaderboard
+
+            fetchLeaderboard().then((leaderboard) => {
+                console.log(leaderboard);
+                leaderboard = leaderboard || [];
+                const myName = document.getElementById("name").value || "Unknown"
+                leaderboard.push({name: myName, score: score, playedAt: new Date().toLocaleString(), difficulty: 
+                    document.querySelector("#difficulty").options[document.querySelector("#difficulty").selectedIndex].text
+                });
+                console.log(leaderboard, "bsort");
+                leaderboard.sort((a, b) => b.score - a.score);
+                console.log(leaderboard, "asort");
+                saveLeaderboard(leaderboard);
+                // Sort leaderboard by score
+                // Display leaderboard
             const leaderboardTable = document.querySelector("#leaderboardTable tbody");
             leaderboardTable.innerHTML = "";
             for (let i = 0; i < leaderboard.length; i++) {
@@ -73,7 +120,7 @@ function runTimer()
                     `;
                 leaderboardTable.appendChild(row);
             }
-            
+            })
         }
         
     },1000);
@@ -103,3 +150,5 @@ document.querySelector("#pbtm").addEventListener("click",function(details){
     }
 })
 
+document.getElementById("startBtn").addEventListener("click", playStart);
+restartBtn.addEventListener("click", playStart);
